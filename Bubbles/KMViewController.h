a2dd0cc8422dbdb411189213da2dd0ab1db9e2a8
kellimohr <kellimohr@gmail.com>
2013-11-01 19:56:16 -0700

    Initial Commit

---
+++ Bubbles/KMViewController.h
@@ -0,0 +1,13 @@
+//
+//  KMViewController.h
+//  Bubbles
+//
+//  Created by Kelli Mohr on 11/1/13.
+//  Copyright (c) 2013 Kelli Mohr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMViewController : UIViewController
+
+@end
---
+++ Bubbles/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Bubbles
+//
+//  Created by Kelli Mohr on 11/1/13.
+//  Copyright (c) 2013 Kelli Mohr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KMAppDelegate class]));
+    }
+}
